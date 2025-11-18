b53a2cb916ba67d044863e64d01da9ead90530d2
syedsufiyanulla7-hub <syedsufiyanulla7@gmail.com>
2025-11-18 11:31:43 +0530

    Create While statement.c

---
+++ While statement.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int i = 1;      // initialization
+
+    while (i <= 10) {   // condition
+        printf("%d\n", i); 
+        i++;            // increment
+    }
+
+    return 0;
+}
