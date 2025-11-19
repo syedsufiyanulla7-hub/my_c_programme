94f90b31a3963b05604a585fe3321015cb0fe5ce
syedsufiyanulla7-hub <syedsufiyanulla7@gmail.com>
2025-11-19 12:24:36 +0530

    Create Sum of array elements.c

---
+++ Sum of array elements.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+    int pen[4];
+    int sum=0;
+    printf("enter the elements\n");
+    for (int i=0;i<4;i++){
+        scanf("enter the num:%d",& pen[i]);
+        sum+=pen[i];
+    }
+       
+    
+        ("%d",sum);  
+}
+
